db2d14ff96c9cabe8f3663ae88477991111d9d42
Alex <alex11535@bezeqint.net>
2019-03-26 21:58:57 +0200

    Initial commit

---
+++ desktop/src/com/obstacle/alex/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.obstacle.alex.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.obstacle.alex.MainObstacle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainObstacle(), config);
+	}
+}
