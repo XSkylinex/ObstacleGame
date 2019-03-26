ac044b653d0b2bf68aa5c64fa8f62af740632de1
Alex <alex11535@bezeqint.net>
2019-03-27 01:53:48 +0200

    add player move need add touch screen!

--- core/src/com/obstacle/alex/screen/GameScreen.java
+++ core/src/com/obstacle/alex/screen/GameScreen.java
@@ -3,9 +3,11 @@ package com.obstacle.alex.screen;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.obstacle.alex.config.GameConfig;
+import com.obstacle.alex.entity.Player;
 import com.obstacle.alex.util.GdxUtils;
 import com.obstacle.alex.util.ViewportUtils;
 
@@ -15,17 +17,30 @@ public class GameScreen implements Screen {
     private Viewport viewport;
     private ShapeRenderer renderer;
 
+    private static final Logger log = new Logger(GameScreen.class.getName(),Logger.DEBUG);
+
+    private Player player;
+
     @Override
     public void show () { // show it like create initialize game and load resources
         this.camera = new OrthographicCamera();
         this.viewport = new FillViewport(GameConfig.WORLD_WIDTH,GameConfig.WORLD_HEIGHT,camera);
         this.renderer = new ShapeRenderer();
+        this.player = new Player();
+
+        float startPlayerX = GameConfig.WORLD_WIDTH/2f;
+        float startPlayerY = 1;
+
+        player.setPosition(startPlayerX,startPlayerY);
     }
 
     @Override
     public void render (float delta) {
+        update(delta);
+
         GdxUtils.clearScreen();
-        drewDebug();
+
+        renderDebug();
     }
 
     @Override
@@ -35,7 +50,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width,height);
+        viewport.update(width,height,true);
         ViewportUtils.debugPixelPerUnit(viewport);
     }
 
@@ -54,9 +69,26 @@ public class GameScreen implements Screen {
         dispose();
     }
 
-
-
     private void drewDebug(){
+        player.drawDebug(renderer);
+    }
+
+
+    private void renderDebug(){
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        drewDebug();
+        renderer.end();
         ViewportUtils.drawGrid(viewport,renderer);
+    }
+
+
+    private void update(float delta){
+        updatePlyer();
+    }
+
+    private void updatePlyer(){
+        log.debug("PlayerX= " +player.getX() +" PlayerY= " + player.getY());
+        player.update();
     }
 }
